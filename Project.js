d59be59a4959732d85437a91fb8df9ca37c4712c
Jorgan97 <morganjo1199@gmail.com>
2024-07-08 18:45:19 -0400

    Added project to Git

---
+++ Project.js
@@ -0,0 +1,8 @@
+class Project {
+    constructor(title, subtitle, description, tags) {
+        this.title = title;
+        this.subtitle = subtitle;
+        this.description = description;
+        this.tags = tags;
+    }
+}
---
+++ script.js
@@ -0,0 +1,12 @@
+// class Project {
+//     constructor(title, subtitle, description, tags) {
+//         this.title = title;
+//         this.subtitle = subtitle;
+//         this.description = description;
+//         this.tags = tags;
+//     }
+// }
+async function populate() {
+    const requestURL = "https://github.com/Jorgan97/J.-Morgan-Project-Portfolio/blob/521d4b2e6663935ef587b3ddb73673604a16493b/projects.json";
+    
+}
