a0a44e8fd1faf5f6667babcd428ead40f4d941ed
Jorgan97 <morganjo1199@gmail.com>
2024-07-09 00:10:06 -0400

    First testing of parsing json objects

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 main();
 
 function main() {
-    populate();
+    loadProjects();
 }
-
-async function populate() {
+async function loadProjects() {
     fetch("Projects.json")
     .then(response => response.json())
-    .then(jsonResponse => console.log(jsonResponse))
+    .then(jsonResponse => createObjects(jsonResponse))
+}
+function createObjects(jsonResponse) {
+    let projectKeys = Object.keys(jsonResponse);
+    let projects = [];
+    for (var i = 0; i < projectKeys.length - 1; i++) {
+        projects.push(jsonResponse[projectKeys[i]]);
+    }
+    console.log(projects);
+}
+function listProjects() {
+    const projTable = document.querySelector("#projectTable");
+    projTable.innerHTML += '';
+
 }
