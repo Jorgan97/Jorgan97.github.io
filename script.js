4cdb8232721aa45381e6b857b4fc7eb04d0a5ff1
Jorgan97 <morganjo1199@gmail.com>
2024-07-08 23:10:35 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 main();
 
+function main() {
+    populate();
+}
+
 async function populate() {
     fetch("Projects.json")
     .then(response => response.json())
     .then(jsonResponse => console.log(jsonResponse))
-}
-
-function main() {
-    populate();
 }
